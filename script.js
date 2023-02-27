f74721d46aca4e0a7e8b9e3ee9cb6d8603d4a948
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-02-27 20:52:48 -0300

    Início do código Javascript.

---
+++ script.js
@@ -0,0 +1,25 @@
+const currentDate = document.querySelector(".current-date");
+
+
+// Pegando a data, mês e ano atuais:
+let date = new Date();
+currentYear = date.getFullYear();
+currentMonth = date.getMonth();
+
+
+//Meses:
+let monthsBrazil = new Array("Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Septembro", "Outubro", "Novembro", "Dezembro");
+
+
+const showCalendar = () => {
+
+currentDate
+
+}
+
+monthsBrazil.forEach((monthsBrazil) =>{
+
+
+}) 
+
+showCalendar();
