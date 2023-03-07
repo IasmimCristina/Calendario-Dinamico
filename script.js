b6f9ad2aeb15cb332b207433b749fc4b8a89ce29
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-03-07 19:40:15 -0300

    Problema com getmonth() resolvido.

--- script.js
+++ script.js
@@ -6,45 +6,52 @@ const currentDate = document.querySelector(".current-date"),
 
 
 // Pegando a data, mês e ano atuais:
-let date = new Date();
-currentYear = date.getFullYear();
-currentMonth = date.getMonth();
-let monthName;
+let date = new Date(),
+  currentYear = date.getFullYear(),
+  currentMonth = date.getMonth();
+
 
 //Meses:
 const monthsBrazil = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 
-
+//FAÇA A CORREÇÃO!!!
 
 
 const showCalendar = () => {
 
-  let firstDayMonth = new Date(currentYear, currentMonth,   1).getDay(),  //Baseado na ordem do dia de semana. 
-  lastDayMonth = new Date(currentYear, currentMonth + 1, 0).getDate(), // Observação: se você não usar o +1, será feito o cálculo com o mês passado.  
-  lastDayLastMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-  
+  let firstDayMonth = new Date(currentYear, currentMonth, 1).getDay(),  //Baseado na ordem do dia de semana. 
+    lastDateMonth = new Date(currentYear, currentMonth + 1, 0).getDate(), // Observação: se você não usar o +1, será feito o cálculo com o mês passado.  
+    lastDayMonth = new Date(currentYear, currentMonth, lastDateMonth).getDay(), //Obtendo o último dia na semana do mês.
+    lastDayLastMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
   let liTag = "";
 
   //Linha superior.
   for (let i = firstDayMonth; i > 0; i--) {
-    liTag += `<li class="inactive">${lastDayLastMonth - i + 1}</li>`;
-    
+    liTag += `<li class="inactive">${lastDayLastMonth - i + 1}</li>`; //Dias inativos do próximo mês.
+
   }
 
   // Números na tela:
-  for (let i = 1; i <= lastDayMonth; i++) {
-    liTag += `<li>${i}</li>`; //Faz com que todos os números do mês apareçam. 
+  for (let i = 1; i <= lastDateMonth; i++) {
+    let isToday = i === date.getDate() && currentMonth === new Date().getMonth()
+      && currentYear === new Date().getFullYear() ? "active" : " ";
+
+    liTag += `<li class=" ${isToday} ">${i}</li>`; //Faz com que todos os números do mês apareçam. 
 
   }
 
   //Linha inferior.
+  for (let i = lastDayMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayMonth + 1}</li>`;
+
+  }
 
 
-  
   currentDate.innerText = `${monthsBrazil[currentMonth]} - ${currentYear}`;
   daysTag.innerHTML = liTag;
- 
+
 
 }
 function prevNextChange() {
@@ -54,7 +61,7 @@ function prevNextChange() {
     icon.addEventListener("click", () => {
       currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
       showCalendar();
-    })  
+    })
   })
 }
 
